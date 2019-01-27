5f407de833b1729ccd9e591e1778f7e90e7420a7
Benjamin Mollenhauer <dev@mollbe.de>
2019-01-27 11:02:52 +0000

    better types for default export

--- src/arangojs.ts
+++ src/arangojs.ts
@@ -4,11 +4,10 @@ import { Config } from "./connection";
 import { Database } from "./database";
 import { ArangoError } from "./error";
 
-export default function arangojs(config: Config) {
+function arangojs(config: Config) {
   return new Database(config);
 }
-
-Object.assign(arangojs, { CollectionType, ArangoError, Database, aql });
+export default Object.assign(arangojs, { CollectionType, ArangoError, Database, aql });
 export { DocumentCollection, EdgeCollection } from "./collection";
 export { Graph } from "./graph";
 export { Database, aql };
